2c0595f5309d3ded9782dfbc990b3153a9f1f4de
Richard Taylor <moomerman@gmail.com>
2017-06-14 22:17:18 +0100

    Dynamically lauching applications on request

---
+++ dev/app.go
@@ -0,0 +1,140 @@
+package dev
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"os/exec"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/moomerman/phx-dev/multiproxy"
+	"github.com/puma/puma-dev/homedir"
+	"github.com/vektra/errors"
+)
+
+const appsPath = "~/.phx-dev"
+
+var apps map[string]*App
+var lock sync.Mutex
+
+type App struct {
+	Host    string
+	Port    string
+	Link    string
+	Dir     string
+	Command *exec.Cmd
+
+	proxy  *multiproxy.MultiProxy
+	stdout io.Reader
+}
+
+func (a *App) Start() error {
+	// take the hostname, see if we have a symlink that matches
+	// if so, follow and determine the dir of the application
+
+	path := homedir.MustExpand(appsPath) + "/" + a.Host
+	stat, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+
+	dir, err := os.Readlink(path)
+	if err != nil {
+		return err
+	}
+
+	if stat.IsDir() {
+		a.Link = path
+		a.Dir = dir
+	} else {
+		return errors.New("unknown app")
+	}
+
+	return a.launch()
+}
+
+const executionShell = `exec bash -c '
+cd %s
+exec mix phx.server'
+`
+
+func (a *App) launch() error {
+	shell := os.Getenv("SHELL")
+	addr := findAvailableAddr()
+	_, port, err := net.SplitHostPort(addr)
+	a.Port = port
+
+	cmd := exec.Command(shell, "-l", "-i", "-c",
+		fmt.Sprintf(executionShell, a.Dir))
+
+	cmd.Dir = a.Dir
+
+	cmd.Env = os.Environ()
+	cmd.Env = append(cmd.Env,
+		fmt.Sprintf("PHX_PORT=%s", a.Port),
+	)
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	a.stdout = stdout
+	cmd.Stderr = cmd.Stdout
+
+	err = cmd.Start()
+	if err != nil {
+		return errors.Context(err, "starting app")
+	}
+
+	a.Command = cmd
+	a.proxy = multiproxy.NewProxy("http://127.0.0.1:"+a.Port, a.Host)
+
+	time.Sleep(5 * time.Second)
+
+	return nil
+}
+
+func findAppForHost(host string) (*App, error) {
+	lock.Lock()
+	defer lock.Unlock()
+
+	hostParts := strings.Split(host, ":")
+	host = hostParts[0]
+
+	if apps == nil {
+		apps = make(map[string]*App)
+	}
+
+	app := apps[host]
+	if app != nil {
+		return app, nil
+	}
+
+	fmt.Println("[app] attempting to start app for host", host)
+
+	app = &App{
+		Host: host,
+	}
+
+	err := app.Start()
+	if err != nil {
+		fmt.Println("[app] error starting app for host", host, err)
+		return nil, err
+	}
+
+	fmt.Println("[app] created app for host", host, app.Port)
+	// TODO: apps should be keyed by Dir not host as you might have multiple
+	// hosts pointing to the same app
+	apps[host] = app
+	return app, nil
+}
+
+func findAvailableAddr() string {
+	l, _ := net.Listen("tcp", ":0")
+	l.Close()
+	return l.Addr().String()
+}
--- dev/server.go
+++ dev/server.go
@@ -7,16 +7,11 @@ import (
 	"net/http"
 
 	"github.com/moomerman/phx-dev/devcert"
-	"github.com/moomerman/phx-dev/multiproxy"
 	"golang.org/x/net/http2"
 )
 
-var proxy *multiproxy.MultiProxy
-
 // Start starts the HTTP and HTTPS proxy servers
 func Start() {
-	proxy = multiproxy.NewProxy("http://127.0.0.1:4000", "localhost")
-
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", proxyHandler())
 
@@ -58,8 +53,15 @@ func proxyHandler() func(http.ResponseWriter, *http.Request) {
 		if r.TLS != nil {
 			scheme = "https"
 		}
+
+		app, err := findAppForHost(r.Host)
+		if err != nil {
+			// render an error
+			return
+		}
+
 		source := fmt.Sprint(r.Method, " ", r.Proto, " ", scheme+"://", r.Host, r.URL)
-		fmt.Println("[proxy]", source, "->", proxy.URL)
-		proxy.Proxy(w, r)
+		fmt.Println("[proxy]", source, "->", app.proxy.URL)
+		app.proxy.Proxy(w, r)
 	}
 }
