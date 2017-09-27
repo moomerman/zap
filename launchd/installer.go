5c18c025cfee652a10b49432cec7202870ee6e91
Richard Taylor <moomerman@gmail.com>
2017-09-27 23:20:01 +0100

    Move launchd installer to separate package

---
+++ launchd/installer.go
@@ -0,0 +1,102 @@
+package launchd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+
+	"github.com/kardianos/osext"
+	"github.com/puma/puma-dev/homedir"
+	"github.com/vektra/errors"
+)
+
+// Install installs the launch agent on macOS
+func Install(appID, appName string, httpPort, tlsPort int) error {
+	binPath, err := osext.Executable()
+	if err != nil {
+		return errors.Context(err, "calculating executable path")
+	}
+
+	fmt.Printf("* Use '%s' as the location of %s\n", binPath, appName)
+
+	var userTemplate = `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+   <key>Label</key>
+   <string>%s</string>
+   <key>ProgramArguments</key>
+   <array>
+	     <string>%s</string>
+			 <string>-launchd</string>
+   </array>
+   <key>KeepAlive</key>
+   <true/>
+   <key>RunAtLoad</key>
+   <true/>
+   <key>Sockets</key>
+   <dict>
+       <key>Socket</key>
+       <dict>
+           <key>SockNodeName</key>
+           <string>0.0.0.0</string>
+           <key>SockServiceName</key>
+           <string>%d</string>
+       </dict>
+       <key>SocketTLS</key>
+       <dict>
+           <key>SockNodeName</key>
+           <string>0.0.0.0</string>
+           <key>SockServiceName</key>
+           <string>%d</string>
+       </dict>
+   </dict>
+   <key>StandardOutPath</key>
+   <string>%s</string>
+   <key>StandardErrorPath</key>
+   <string>%s</string>
+</dict>
+</plist>
+`
+
+	logPath := homedir.MustExpand("~/Library/Logs/" + appName + ".log")
+	plistDir := homedir.MustExpand("~/Library/LaunchAgents")
+	plist := homedir.MustExpand("~/Library/LaunchAgents/" + appName + ".plist")
+
+	config := []byte(fmt.Sprintf(userTemplate, appName, binPath, httpPort, tlsPort, logPath, logPath))
+
+	if err := os.MkdirAll(plistDir, 0644); err != nil {
+		return errors.Context(err, "creating LaunchAgents directory")
+	}
+
+	if err := ioutil.WriteFile(plist, config, 0644); err != nil {
+		return errors.Context(err, "writing LaunchAgent plist")
+	}
+
+	exec.Command("launchctl", "unload", plist).Run()
+
+	if err := exec.Command("launchctl", "load", plist).Run(); err != nil {
+		return errors.Context(err, "launchctl load <plist>")
+	}
+
+	fmt.Printf("* Installed %s on ports: http %d, https %d\n", appID, httpPort, tlsPort)
+
+	return nil
+}
+
+// Uninstall removes the launch agent on macOS
+func Uninstall(appID, appName string) error {
+	plist := homedir.MustExpand("~/Library/LaunchAgents/" + appName + ".plist")
+
+	if err := exec.Command("launchctl", "unload", plist).Run(); err != nil {
+		return errors.Context(err, "launchctl unload <plist>")
+	}
+
+	if err := os.Remove(plist); err != nil {
+		return errors.Context(err, "removing LaunchAgent plist")
+	}
+
+	fmt.Printf("* Removed %s from automatically running\n", appID)
+	return nil
+}
--- zap/install_darwin.go
+++ zap/install_darwin.go
@@ -1,107 +1,24 @@
 package zap
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
-	"os/exec"
-
-	"github.com/kardianos/osext"
-	"github.com/puma/puma-dev/homedir"
-	"github.com/vektra/errors"
+	"github.com/moomerman/zap/launchd"
 )
 
-const applicationName = "com.github.moomerman.zapd"
-const applicationShortName = "zapd"
+const appID = "com.github.moomerman.zapd"
+const appName = "zapd"
 
 // Install installs the launch agent on macOS
 func Install(httpPort, tlsPort int) error {
-	binPath, err := osext.Executable()
-	if err != nil {
-		return errors.Context(err, "calculating executable path")
+	if err := launchd.Install(appID, appName, httpPort, tlsPort); err != nil {
+		return err
 	}
-
-	fmt.Printf("* Use '%s' as the location of zap\n", binPath)
-
-	var userTemplate = `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-   <key>Label</key>
-   <string>%s</string>
-   <key>ProgramArguments</key>
-   <array>
-	     <string>%s</string>
-			 <string>-launchd</string>
-   </array>
-   <key>KeepAlive</key>
-   <true/>
-   <key>RunAtLoad</key>
-   <true/>
-   <key>Sockets</key>
-   <dict>
-       <key>Socket</key>
-       <dict>
-           <key>SockNodeName</key>
-           <string>0.0.0.0</string>
-           <key>SockServiceName</key>
-           <string>%d</string>
-       </dict>
-       <key>SocketTLS</key>
-       <dict>
-           <key>SockNodeName</key>
-           <string>0.0.0.0</string>
-           <key>SockServiceName</key>
-           <string>%d</string>
-       </dict>
-   </dict>
-   <key>StandardOutPath</key>
-   <string>%s</string>
-   <key>StandardErrorPath</key>
-   <string>%s</string>
-</dict>
-</plist>
-`
-
-	logPath := homedir.MustExpand("~/Library/Logs/" + applicationName + ".log")
-	plistDir := homedir.MustExpand("~/Library/LaunchAgents")
-	plist := homedir.MustExpand("~/Library/LaunchAgents/" + applicationName + ".plist")
-
-	err = os.MkdirAll(plistDir, 0644)
-
-	if err != nil {
-		return errors.Context(err, "creating LaunchAgent directory")
-	}
-
-	err = ioutil.WriteFile(
-		plist,
-		[]byte(fmt.Sprintf(userTemplate, applicationName, binPath, httpPort, tlsPort, logPath, logPath)),
-		0644,
-	)
-
-	if err != nil {
-		return errors.Context(err, "writing LaunchAgent plist")
-	}
-
-	exec.Command("launchctl", "unload", plist).Run()
-
-	err = exec.Command("launchctl", "load", plist).Run()
-	if err != nil {
-		return errors.Context(err, "loading plist into launchctl")
-	}
-
-	fmt.Printf("* Installed %s on ports: http %d, https %d\n", applicationShortName, httpPort, tlsPort)
-
 	return nil
 }
 
 // Uninstall removes the launch agent on macOS
-func Uninstall() {
-	plist := homedir.MustExpand("~/Library/LaunchAgents/" + applicationName + ".plist")
-
-	exec.Command("launchctl", "unload", plist).Run()
-
-	os.Remove(plist)
-
-	fmt.Printf("* Removed %s from automatically running\n", applicationShortName)
+func Uninstall() error {
+	if err := launchd.Uninstall(appID, appName); err != nil {
+		return err
+	}
+	return nil
 }
