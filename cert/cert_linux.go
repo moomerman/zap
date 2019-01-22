5b085c7702effc59789430057ad896408390b90b
Richard Taylor <moomerman@gmail.com>
2019-01-22 15:14:16 +0000

    wip - get server running, instructions for installing ssl cert

--- cert/cert_linux.go
+++ cert/cert_linux.go
@@ -2,7 +2,7 @@ package cert
 
 import "fmt"
 
-const supportDir = "~/.puma-dev-ssl"
+const supportDir = "~/.zap/ssl"
 
 // InstallCert installs a CA certificate root in the system cacerts on linux
 func InstallCert(cert string) error {
@@ -13,3 +13,42 @@ func InstallCert(cert string) error {
 	// lsb_release -a
 	// cat /etc/*release
 }
+
+// https://thomas-leister.de/en/how-to-import-ca-root-certificate/
+
+// #!/bin/bash
+
+// ### Script installs root.cert.pem to certificate trust store of applications using NSS
+// ### (e.g. Firefox, Thunderbird, Chromium)
+// ### Mozilla uses cert8, Chromium and Chrome use cert9
+
+// ###
+// ### Requirement: apt install libnss3-tools
+// ###
+
+// ###
+// ### CA file to install (CUSTOMIZE!)
+// ###
+
+// certfile="cert.pem"
+// certname="ZAP CA"
+
+// ###
+// ### For cert8 (legacy - DBM)
+// ###
+
+// for certDB in $(find ~/ -name "cert8.db")
+// do
+//     certdir=$(dirname ${certDB});
+//     certutil -A -n "${certname}" -t "TCu,Cu,Tu" -i ${certfile} -d dbm:${certdir}
+// done
+
+// ###
+// ### For cert9 (SQL)
+// ###
+
+// for certDB in $(find ~/ -name "cert9.db")
+// do
+//     certdir=$(dirname ${certDB});
+//     certutil -A -n "${certname}" -t "TCu,Cu,Tu" -i ${certfile} -d sql:${certdir}
+// done
--- zap/server.go
+++ zap/server.go
@@ -4,13 +4,11 @@ import (
 	"context"
 	"crypto/tls"
 	"log"
-	"net"
 	"net/http"
 	"sync"
 	"time"
 
 	"github.com/moomerman/zap/cert"
-	"github.com/puma/puma-dev/dev/launch"
 	"golang.org/x/net/http2"
 )
 
@@ -57,40 +55,6 @@ func (s *Server) Stop() {
 	s.https.Shutdown(ctx)
 }
 
-func (s *Server) serveHTTP() error {
-	var listener net.Listener
-	var err error
-
-	if s.HTTPAddr == "Socket" {
-		listener = getSocketListener(s.HTTPAddr)
-	} else {
-		listener, err = net.Listen("tcp", s.HTTPAddr)
-		if err != nil {
-			log.Fatal("[zap] unable to create listener", err)
-		}
-	}
-
-	log.Println("[zap] http listening at", listener.Addr())
-	return s.http.Serve(listener)
-}
-
-func (s *Server) serveHTTPS() error {
-	var listener net.Listener
-	var err error
-
-	if s.HTTPSAddr == "SocketTLS" {
-		listener = tls.NewListener(getSocketListener(s.HTTPSAddr), s.https.TLSConfig)
-	} else {
-		listener, err = tls.Listen("tcp", s.HTTPSAddr, s.https.TLSConfig)
-		if err != nil {
-			log.Fatal("[zap] unable to create tls listener", err)
-		}
-	}
-
-	log.Println("[zap] https listening at", listener.Addr())
-	return s.https.Serve(listener)
-}
-
 func createHTTPServer() *http.Server {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", findAppHandler(appHandler))
@@ -130,12 +94,4 @@ func createHTTPSServer() *http.Server {
 	http2.ConfigureServer(server, nil)
 
 	return server
-}
-
-func getSocketListener(socket string) net.Listener {
-	listeners, err := launch.SocketListeners(socket)
-	if err != nil {
-		log.Fatal("[zap] unable to get launchd socket listener", err)
-	}
-	return listeners[0]
 }
